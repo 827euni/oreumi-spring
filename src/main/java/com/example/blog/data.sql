6dcbd2f44093711257fe3398e9c9909ed0ebe0c1
euna kwon <eunakwon@naver.com>
2024-03-20 17:55:15 +0900

    📝 사용 테이블 변경

--- src/main/java/com/example/blog/data.sql
+++ src/main/java/com/example/blog/data.sql
@@ -1,9 +1,14 @@
-create table article (
-                         id BIGINT AUTO_INCREMENT primary key,
-                         title varchar(255) not null,
-                         content varchar(255) not null
+CREATE TABLE article (
+                         id BIGINT AUTO_INCREMENT PRIMARY KEY,
+                         title VARCHAR(255) NOT NULL,
+                         content VARCHAR(255) NOT NULL,
+                         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+                         updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-INSERT INTO article (title, content) VALUES ('블로그 제목', '블로그 내용');
-INSERT INTO article (title, content) VALUES ('블로그 제목2', '블로그 내용2');
-INSERT INTO article (title, content) VALUES ('블로그 제목3', '블로그 내용3');
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목1', '내용1', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목2', '내용2', NOW(), NOW());
+INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목3', '내용3', NOW(), NOW());
+
+
+
