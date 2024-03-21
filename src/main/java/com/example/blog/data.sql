7848f3ec9358a0d673ab56472d142d59c111cf73
euna kwon <eunakwon@naver.com>
2024-03-21 16:51:29 +0900

    📝 SQL문 CREATE 제외 제거

--- src/main/java/com/example/blog/data.sql
+++ src/main/java/com/example/blog/data.sql
@@ -6,11 +6,6 @@ CREATE TABLE article (
                          updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목1', '내용1', NOW(), NOW());
-INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목2', '내용2', NOW(), NOW());
-INSERT INTO article (title, content, created_at, updated_at) VALUES ('제목3', '내용3', NOW(), NOW());
-
-
 CREATE TABLE users (
                        id BIGINT AUTO_INCREMENT PRIMARY KEY,
                        email VARCHAR(255) NOT NULL UNIQUE,
